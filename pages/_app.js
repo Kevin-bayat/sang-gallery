5815866a920a1433efe9e860d395bbf9168ce129
online-collage <137940891+online-collage@users.noreply.github.com>
2023-08-24 15:08:37 +0330

    add sass to project

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
-import '../styles/globals.css'
+import "../shared/styles/globals.scss";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta
+          httpEquiv="Content-Type"
+          content="Type=text/html; charset=UTF-8"
+        />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta property="og:locale" content="fa_IR" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+        <meta name="format-detection" content="telephone=no" />
+        <meta property="og:type" content="website" />
+        <link rel="icon" href="/sang-logo.png" />
+        <title>گالری سنگ</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
